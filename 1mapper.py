73c3fe2ec2934de54545d1de83ad01703c029891
mohammadfarheen <46694155+mohammadfarheen@users.noreply.github.com>
2020-02-22 12:17:06 -0600

    Add files via upload

--- 1mapper.py
+++ 1mapper.py
@@ -4,10 +4,7 @@ o = open("01.txt", "w")
 for line in i:
     datalist = line.strip().split("\t")
     BookID, title, authors, average_rating, isbn, isbn13, language_code,  num_pages, ratings_count, text_reviews_count = datalist
-    o.write(authors + "\t" + average_rating  + "\n")
+    o.write(title + "\t" + authors  + "\n")
 
 i.close()
 o.close()
-
-
-
